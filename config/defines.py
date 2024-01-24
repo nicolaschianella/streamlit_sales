4a1a3d60eebbfd078437b6d03b672007f8eca23e
nicolaschianella <chianella.nicolas.3c@gmail.com>
2024-01-24 21:11:34 +0100

    Added global variables definitions

---
+++ config/defines.py
@@ -0,0 +1,13 @@
+###############################################################################
+#
+# File:      defines.py
+# Author(s): Nico
+# Scope:     Global variables to use within the project
+#
+# Created:   24 January 2024
+#
+###############################################################################
+# API Host (port handled in entry point parameters)
+API_HOST = "http://127.0.0.1"
+# Route to get_clothes
+GET_CLOTHES_ROUTE = "api/operations/get_clothes"
