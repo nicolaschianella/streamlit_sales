fa28593e906db5bc80952d0d4c92a8c8cb2321b2
nicolaschianella <chianella.nicolas.3c@gmail.com>
2024-01-26 18:18:18 +0100

    Added example of clothes loading using multiple requests

--- 1_Recherche_vêtements.py
+++ 1_Recherche_vêtements.py
@@ -15,6 +15,8 @@ import argparse
 import os
 import time
 from typing import Union
+from datetime import datetime
+from operator import itemgetter
 
 from config.defines import API_HOST, GET_CLOTHES_ROUTE
 
@@ -29,21 +31,46 @@ def run() -> None:
 def get_clothes(port: int) -> Union[str, requests.models.Response]:
     """Main function called when we click on the "Chercher vêtements" button.
     Acquires Vinted clothes using our API.
+    :param port: int, API port to use
     :return: clothes, str (in case of error) or requests.models.Response (if the call was successful)"""
     # Loading spinner
     with st.spinner('Acquisition des vêtements...'):
         try:
-            # TODO: add parameters, loop or threads for each request
-            clothes = requests.get(f"{API_HOST}:{port}/{GET_CLOTHES_ROUTE}", data=json.dumps({}))
+            # TODO: load selected requests and loop over them instead
+            clothes = []
+            for _ in range(2):
+                request_clothes = requests.get(f"{API_HOST}:{port}/{GET_CLOTHES_ROUTE}", data=json.dumps({}))
+                clothes = format_clothes(clothes, request_clothes)
+
+            # Finally sort by datetime
+            clothes = sorted(clothes, key=itemgetter('created_at_datetime'), reverse=True)
+
         except requests.exceptions.ConnectionError:
             clothes = "Oops! L'API semble down."
     # State is not run anymore
     st.session_state.run = False
     return clothes
 
+def format_clothes(clothes: list, request_clothes: requests.models.Response) -> list:
+    """
+    Formats clothes from the API response
+    :param clothes: list, all the current clothes
+    :param request_clothes: requests.models.Response, API response for the given request
+    :return: list, formatted clothes with unique values
+    """
+    request_clothes = json.loads(request_clothes.json()["data"])
+    for  item in request_clothes:
+        # Format str to datetime
+        # TODO: change timezoneo Europe/Brussels and handle case where we have "NA" in item["created_at_ts"] (interpolation)
+        item["created_at_datetime"] = datetime.strptime(item["created_at_ts"], f"%Y-%m-%dT%H:%M:%S%z")
+        if item not in clothes:
+            clothes.append(item)
+
+    return clothes
+
 def main(port: int) -> None:
     """Main function running this page.
-    :param port: API port to use
+    :param port: int, API port to use
     :return: None
     """
     # Deactivate button when running get_clothes
@@ -66,7 +93,7 @@ def main(port: int) -> None:
 
         else:
             # Case call successful -> TODO: format data and display
-            st.write(st.session_state.result.json())
+            st.write(st.session_state.result)
 
 
 
