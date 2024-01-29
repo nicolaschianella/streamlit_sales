3ca1fe77f5d0b44d25ea782338e003a0c4cdbed9
nicolaschianella <chianella.nicolas.3c@gmail.com>
2024-01-29 16:58:25 +0100

    Added requests names mapper

--- utils/defines.py
+++ utils/defines.py
@@ -13,3 +13,15 @@ API_HOST = "http://127.0.0.1"
 GET_CLOTHES_ROUTE = "api/operations/get_clothes"
 # Route to get_requests
 GET_REQUESTS_ROUTE = "api/operations/get_requests"
+# Mapper {fields_to_be_displayed: displayed_value} for requests edition
+# These will also be the available fields to fill in to create a new request
+MAPPER_REQUESTS = {
+    "name": "Nom de la recherche",
+    "creation_date": "Date de création",
+    "per_page": "Nb d'articles à chercher",
+    "search_text": "Mots clés",
+    "brand_ids": "Marque",
+    "price_from": "Prix minimum",
+    "price_to": "Prix maximum",
+    "status_ids": "Etat de l'article"
+}
