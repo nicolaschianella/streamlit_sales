91f4446ec0a670ece44f218ef297e6f217539e7f
nicolaschianella <chianella.nicolas.3c@gmail.com>
2024-02-03 18:16:16 +0100

    Added references brands and clothes states, changed MAPPER_REQUESTS displayed names, add columns config

--- utils/defines.py
+++ utils/defines.py
@@ -7,6 +7,9 @@
 # Created:   24 January 2024
 #
 ###############################################################################
+import streamlit as st
+
+
 # API Host (port handled in entry point parameters)
 API_HOST = "http://127.0.0.1"
 # Route to get_clothes
@@ -16,12 +19,69 @@ GET_REQUESTS_ROUTE = "api/operations/get_requests"
 # Mapper {fields_to_be_displayed: displayed_value} for requests edition
 # These will also be the available fields to fill in to create a new request
 MAPPER_REQUESTS = {
-    "name": "Nom de la recherche",
+    # _id is the only exception and will not be displayed
+    "_id": "_id",
+    "name": "Nom",
     "creation_date": "Date de création",
-    "per_page": "Nb d'articles à chercher",
+    "per_page": "Nb articles",
     "search_text": "Mots clés",
     "brand_ids": "Marque",
     "price_from": "Prix minimum",
-    "price_to": "Prix maximum",
-    "status_ids": "Etat de l'article"
+    "price_to": "Prix maximum"
+}
+# status_ids is an exception since we want multiple checkbox columns, one for each clothe state
+STATUS_IDS_KEY = "status_ids"
+# brand_ids as well because we need to map id -> brand name
+BRAND_IDS_KEY = "brand_ids"
+# Define clothes states
+MAPPER_STATUS_IDS = {
+    "Neuf avec étiquette": "6",
+    "Neuf sans étiquette": "1",
+    "Très bon état": "2",
+    "Bon état": "3"
+}
+# Define referenced brands
+BRANDS = {
+    "adidas": "14",
+    "Arc'Teryx": "319730",
+    "arte": "271932",
+    "Burberry": "364",
+    "Carhartt": "362",
+    "C.P. Company": "73952",
+    "Lacoste": "304",
+    "Nike": "53",
+    "Ralph Lauren": "88",
+    "Stone Island": "73306",
+    "Stüssy": "441",
+    "The North Face": "2319",
+}
+# Define config to display requests in "Edition requêtes" - checkboxes automatically added for every key
+# in MAPPER_STATUS_IDS
+CONFIG = {
+    "Nom": st.column_config.TextColumn("Nom",
+                                       help="Nom de la recherche dans 'Recherche vêtements'"),
+
+    "Date de création": st.column_config.TextColumn("Date de création",
+                                                    disabled=True,
+                                                    help="Se mettra à jour automatiquement"),
+
+    "Nb articles": st.column_config.NumberColumn("Nb articles",
+                                                 help="Nombre d'articles par recherche (max 96)",
+                                                 min_value=1,
+                                                 max_value=96),
+
+    "Mots clés": st.column_config.TextColumn("Mots clés",
+                                             help="Recherche mots clés comme sur le site"),
+
+    "Marque": st.column_config.SelectboxColumn("Marque",
+                                               options=list(BRANDS.keys()),
+                                               help="Limité à une seule marque"),
+
+    "Prix minimum": st.column_config.NumberColumn("Prix minimum",
+                                                  min_value=0,
+                                                  help="Ne s'affiche pas si 0"),
+
+    "Prix maximum": st.column_config.NumberColumn("Prix maximum",
+                                                  min_value=0,
+                                                  help="Ne s'affiche pas si 0")
 }
